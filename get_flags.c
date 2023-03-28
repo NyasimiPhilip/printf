74a0fa9b92e96f3cc659542577b275558e6b14db
Wilfred Ashiagbor <45347585+wonka94@users.noreply.github.com>
2023-03-28 05:27:27 +0000

    Update files

--- get_flags.c
+++ get_flags.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
@@ -8,7 +9,7 @@
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
+	/* 1 2 4 8 16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
@@ -22,9 +23,12 @@ int get_flags(const char *format, int *i)
 				flags |= FLAGS_ARR[j];
 				break;
 			}
+
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
+
 	*i = curr_i - 1;
+
 	return (flags);
 }
